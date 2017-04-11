40149fb5ca4d9000296d4e44d1c80552eac8a78d
Ali Muhammed <uniapi@outlook.com>
2017-04-11 06:24:54 +0300

    Modifed [main.c] - modified to suit the updated task

--- hello/main.c
+++ hello/main.c
@@ -6,13 +6,45 @@
 **********************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+#include "goodbye.h"
 
-extern char *hello(void);       /* explicit forward declaration  */
-extern char *goodbye(void);
+#define LIBPATH "LD_LIBRARY_PATH"
+#define LIBHELLO "libhello.so"
 
-main(argc, argv)
-int argc; char *argv[];
+main(int argc, char *argv[])
 {
-    printf("%s\n", hello());    /*  printing: Hello, World       */
-    puts(goodbye());            /*  printing: Goodbye, World     */
+    char *libpath = NULL;                   /*  a good practise for team-projects          */
+    void *libhello = NULL;
+    char *(*hello)(void) = NULL;
+
+	libhello = dlopen(LIBHELLO, RTLD_LAZY); /*  loading libhello.so                        */
+	if (!libhello) {                        /*  checking if libhello.so is loaded          */
+		fprintf(stderr, "error: " LIBHELLO " not found\n");
+		fprintf(stderr, "tip: copy " LIBHELLO " to /usr/lib or if no access privileges\n");
+
+		libpath = getenv(LIBPATH);
+		if (!libpath)                       /*  checking if LD_LIBRARY_PATH exists         */
+			fprintf(stderr, "     export " LIBPATH " variable with the value of its directory\n");
+		else
+			fprintf(stderr, "     add its directory to your " LIBPATH " variable\n");
+
+		exit(1);                            /*  exiting with the error code                */
+	}
+
+	hello = (char *(*)(void))dlsym(libhello, "hello");        /*  loading hello function   */
+	if (!hello) {                           /*  checking if the function is loaded         */
+		fprintf(stderr, "error: broken package\n");
+        fprintf(stderr, "tip: try to rebuild the program\n");
+
+		exit(1);
+	}
+
+	printf("%s\n", hello());                /*  printing: Hello, World    */
+	puts(goodbye());                        /*  printing: Goodbye, World  */
+	
+    dlclose(libhello);                      /*  uploading libhello.so     */
+
+    return 0;                               /*  exiting with the success  */
 }
